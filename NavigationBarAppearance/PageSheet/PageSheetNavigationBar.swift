6f72ecc3eb67b1a653cf4a840684180ffb3b3310
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2021-08-06 11:53:36 +0900

    add class

---
+++ NavigationBarAppearance/PageSheet/PageSheetNavigationBar.swift
@@ -0,0 +1,21 @@
+//
+//  PageSheetNavigationBar.swift
+//  NavigationBarAppearance
+//
+//  Created by jun on 2021/08/06.
+//
+
+import UIKit
+
+final class PageSheetNavigationBar: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupNavigation()
+    }
+    
+    private func setupNavigation() {
+        
+    }
+}
