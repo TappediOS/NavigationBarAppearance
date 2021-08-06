81e8a7aa260aa10b66b9cb91d5a383bad3406ebb
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2021-08-06 12:11:09 +0900

    setup collectionView

--- NavigationBarAppearance/PageSheet/PageSheetNavigationBar.swift
+++ NavigationBarAppearance/PageSheet/PageSheetNavigationBar.swift
@@ -8,15 +8,59 @@
 import UIKit
 
 final class PageSheetNavigationBar: UIViewController {
-    
+    @IBOutlet weak var collectionView: UICollectionView!
+    private let collectionViewViewCellIdentifire = "PageSheetCollectionViewCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupCollectionView()
         setupNavigation()
+    }
+    
+    private func setupCollectionView() {
+        collectionView.backgroundColor = .secondarySystemBackground
+        collectionView.alwaysBounceVertical = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
     private func setupNavigation() {
         
+    }
+}
+
+extension PageSheetNavigationBar: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: collectionViewViewCellIdentifire, for: indexPath)
+        
+        cell.layer.cornerRadius = 16
+        return cell
+    }
+}
+
+extension PageSheetNavigationBar: UICollectionViewDelegateFlowLayout {
+    // MARK: - FlowLayout
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.width - 32, height: 95)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
 }
